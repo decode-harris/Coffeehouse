eb8a7f770e22e0d598638c5285ec7e8608447cc7
Christopher Harris <56790192+decode-harris@users.noreply.github.com>
2021-06-29 03:32:25 +1000

    Wireframe : Color & Layers
    Finalizing color scheme for mobile & tablet devices

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,45 +1,29 @@
-const nav = document.querySelector('#nav .list');
+let nav = document.querySelector('#nav');
 nav.style.width = '0%';
 
 
-// const more = document.querySelector('#more');
-// more.style.display = 'none';
-
-const btn_more = document.querySelector('#button-more');
-btn_more.addEventListener('click', ()=> {
-
-    if (more.style.display === 'none') {
-
-        more.style.display = 'block';
-        btn_more.innerHTML = 'less';
-    }
-    else {
-        more.style.display = 'none';
-        btn_more.innerHTML = 'more';
-    }
-    
-});
-
-// navigation button selector [ nav-button ]
-const button_menu = document.querySelector('#button-menu');
+// nav button [ menu ] selector
+let menu = document.querySelector('#menu');
 
 // click event [ button menu ]
-button_menu.addEventListener('click', ()=> {
+menu.addEventListener('click', ()=> {
 
     // validate [ nav ] display props
     if (nav.style.width != '100%') {
-
-        // assign new [ active ] ID to nav list
-        nav.setAttribute('id', 'active');
         // assign a new width value [ 100 % ]
         nav.style.width = '100%';
-    
+
+        menu.innerHTML = 'x';
+        // test [ validation ]
+        console.log('menu : active');
     }
     else {
-        // revert ID back to default [ empty string ]
-        nav.setAttribute('id', '');
         // revert to default
         nav.style.width = '0%';
-    }
 
+        menu.innerHTML = 'x';
+
+        // test [ default ]
+        console.log('menu : default');
+    }
 });
